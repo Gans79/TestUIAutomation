c7b1723f530933c82da70a698ae42ec085398975
Ganesh <gans79@gmail.com>
2019-03-22 23:58:19 +1100

    compare 2 files

---
+++ Comparefiles/ComparetwofilesTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Comparefiles;
+using NUnit.Framework;
+namespace ComparefilesTests
+{
+    public class ComparetwofilesTests
+    {
+        [Test]
+        public void Countsmismatch()
+        {
+            Comparetwofiles x = new Comparetwofiles();
+
+            List<string> differnce = new List<string>();
+            StreamReader f1 = new StreamReader(@"C:\Users\Ganesh\source\repos\TestUIAutomation\Comparefiles\File1.csv");
+            StreamReader f2 = new StreamReader(@"C:\Users\Ganesh\source\repos\TestUIAutomation\Comparefiles\File2.csv");
+            differnce = x.PerformCompare(f1, f2);
+
+            foreach (var differ in differnce)
+            {
+                Console.WriteLine("differ is {0}", differ);
+            }
+        }
+
+    }
+}
